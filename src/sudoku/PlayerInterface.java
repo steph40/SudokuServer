e17abc3f00babc621e6b6c82d466a74f0e8ed05c
Steph <stephanesq1@gmail.com>
2023-06-21 23:06:14 +0100

    hora

--- src/sudoku/PlayerInterface.java
+++ src/sudoku/PlayerInterface.java
@@ -5,6 +5,7 @@
 package sudoku;
 
 import java.rmi.*;
+import java.time.LocalDateTime;
 
 /**
  *
@@ -15,7 +16,7 @@ public interface PlayerInterface extends Remote{
    public void playerJoined(String username) throws RemoteException;
    public void playerLeft(String username) throws RemoteException;
    
-   public void startGame(int[][] values) throws RemoteException;
+   public void startGame(int[][] values,LocalDateTime horaAtual) throws RemoteException;
    public void playerMove(String username, String score) throws RemoteException;
    
    public void gameEnd(String username) throws RemoteException;
--- src/sudoku/Server.java
+++ src/sudoku/Server.java
@@ -10,79 +10,79 @@ import java.rmi.server.*;
 import java.rmi.registry.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-
 /**
  *
  * @author inoca
  */
 public class Server extends UnicastRemoteObject implements SudokuInterface {
-    
+
     private static ArrayList<Sudoku> games = new ArrayList();
-    
+
     private Map<PlayerInterface, Player> players = new HashMap<>();
-    
+    LocalDateTime horaAtual;
     private static Sudoku selected;
     private static Boolean started = false;
-    
+
     Server() throws RemoteException {
         super();
     }
-    
+
     //lista de jogadores com o nome pelo menos
     public static void main(String[] args) {
         int[][] solved1 = {{5, 3, 4, 6, 7, 8, 9, 1, 2},
-          {6, 7, 2, 1, 9, 5, 3, 4, 8},
-          {1, 9, 8, 3, 4, 2, 5, 6, 7},
-          {8, 5, 9, 7, 6, 1, 4, 2, 3},
-          {4, 2, 6, 8, 5, 3, 7, 9, 1},
-          {7, 1, 3, 9, 2, 4, 8, 5, 6},
-          {9, 6, 1, 5, 3, 7, 2, 8, 4},
-          {2, 8, 7, 4, 1, 9, 6, 3, 5},
-          {3, 4, 5, 2, 8, 6, 1, 7, 9}};
-        
+        {6, 7, 2, 1, 9, 5, 3, 4, 8},
+        {1, 9, 8, 3, 4, 2, 5, 6, 7},
+        {8, 5, 9, 7, 6, 1, 4, 2, 3},
+        {4, 2, 6, 8, 5, 3, 7, 9, 1},
+        {7, 1, 3, 9, 2, 4, 8, 5, 6},
+        {9, 6, 1, 5, 3, 7, 2, 8, 4},
+        {2, 8, 7, 4, 1, 9, 6, 3, 5},
+        {3, 4, 5, 2, 8, 6, 1, 7, 9}};
+
         int[][] unsolved1 = {{5, 0, 4, 6, 7, 8, 9, 1, 2},
-          {6, 7, 2, 1, 9, 5, 3, 0, 8},
-          {1, 9, 8, 3, 4, 2, 5, 6, 7},
-          {8, 0, 9, 7, 6, 1, 4, 2, 3},
-          {4, 2, 6, 8, 5, 3, 7, 9, 1},
-          {7, 1, 3, 9, 2, 4, 8, 5, 6},
-          {9, 6, 1, 5, 3, 7, 2, 8, 4},
-          {2, 8, 7, 4, 1, 9, 6, 3, 5},
-          {3, 4, 5, 2, 8, 6, 1, 7, 9}};
-        
+        {6, 7, 2, 1, 9, 5, 3, 0, 8},
+        {1, 9, 8, 3, 4, 2, 5, 6, 7},
+        {8, 0, 9, 7, 6, 1, 4, 2, 3},
+        {4, 2, 6, 8, 5, 3, 7, 9, 1},
+        {7, 1, 3, 9, 2, 4, 8, 5, 6},
+        {9, 6, 1, 5, 3, 7, 2, 8, 4},
+        {2, 8, 7, 4, 1, 9, 6, 3, 5},
+        {3, 4, 5, 2, 8, 6, 1, 7, 9}};
+
         int[][] unsolved2 = {{5, 0, 4, 6, 7, 8, 9, 1, 2},
-          {6, 7, 2, 1, 9, 5, 3, 4, 8},
-          {1, 9, 8, 3, 4, 2, 5, 6, 7},
-          {8, 5, 9, 7, 6, 1, 4, 2, 3},
-          {4, 2, 6, 8, 5, 3, 7, 9, 1},
-          {7, 1, 3, 9, 2, 4, 8, 5, 6},
-          {9, 6, 1, 5, 3, 7, 2, 8, 4},
-          {2, 8, 7, 4, 1, 9, 6, 3, 5},
-          {3, 4, 5, 2, 8, 6, 1, 7, 9}};
-        
+        {6, 7, 2, 1, 9, 5, 3, 4, 8},
+        {1, 9, 8, 3, 4, 2, 5, 6, 7},
+        {8, 5, 9, 7, 6, 1, 4, 2, 3},
+        {4, 2, 6, 8, 5, 3, 7, 9, 1},
+        {7, 1, 3, 9, 2, 4, 8, 5, 6},
+        {9, 6, 1, 5, 3, 7, 2, 8, 4},
+        {2, 8, 7, 4, 1, 9, 6, 3, 5},
+        {3, 4, 5, 2, 8, 6, 1, 7, 9}};
+
         Sudoku game1 = new Sudoku(solved1, unsolved1);
         Sudoku game2 = new Sudoku(solved1, unsolved2);
-        
+
         games.add(game1);
         games.add(game2);
-        
+
         try {
             Registry reg = LocateRegistry.createRegistry(1099);
             Server serv = new Server();
             reg.bind("sudoku", serv);
 
             System.out.println("Servidor RMI iniciado.\nIP: " + InetAddress.getLocalHost().getHostAddress());
-            
+
         } catch (Exception ex) {
             System.out.println("Erro: " + ex.getMessage());
         }
-        
+
     }
 
     @Override
@@ -92,24 +92,24 @@ public class Server extends UnicastRemoteObject implements SudokuInterface {
                 return false;
             }
         }
-        
+
         Player p = new Player(username);
         players.put(player, p);
-        
+
         notifyPlayerJoined(username);
-        
-        if(started) {
-           p.setStatus(true);
-           player.startGame(selected.getValues());
-           
+
+        if (started) {
+            p.setStatus(true);
+            player.startGame(selected.getValues(), horaAtual);
+
         }
 
         return true;
     }
 
     private synchronized void notifyPlayerJoined(String username) throws RemoteException {
-       for (PlayerInterface i : players.keySet()) {
-           i.playerJoined(username);
+        for (PlayerInterface i : players.keySet()) {
+            i.playerJoined(username);
         }
     }
 
@@ -123,31 +123,30 @@ public class Server extends UnicastRemoteObject implements SudokuInterface {
         int line = move.getLine();
         int col = move.getColumn();
         int val = move.getValue();
-        
+
         Player p = players.get(player);
 
-        if(p.getPlay(line, col) == val) {
+        if (p.getPlay(line, col) == val) {
             return selected.getCorrectValues()[line][col] == val;
-        } 
-        
-        if(selected.getCorrectValues()[line][col] == val) {
+        }
+
+        if (selected.getCorrectValues()[line][col] == val) {
             p.incrementScore();
             String score = p.getScore() + "/" + selected.getScore();
-                
+
             for (PlayerInterface i : players.keySet()) {
                 i.playerMove(p.getName(), score);
             }
-            
-            if(p.getScore() == selected.getScore()) {
-                    started = false;
-                    for (PlayerInterface i : players.keySet()) {
-                players.get(i).resetGame();
+
+            if (p.getScore() == selected.getScore()) {
+                started = false;
+                for (PlayerInterface i : players.keySet()) {
+                    players.get(i).resetGame();
                     i.gameEnd(p.getName());
-            }
-                    
                 }
-            
-            
+
+            }
+
             return true;
         }
         return false;
@@ -157,18 +156,18 @@ public class Server extends UnicastRemoteObject implements SudokuInterface {
     public synchronized void playerReady(PlayerInterface player) throws RemoteException {
         Player p = players.get(player);
         p.setStatus(true);
-        
+
         for (Player pl : players.values()) {
-            if(!pl.status) {
+            if (!pl.status) {
                 return;
             }
         }
-        
+
         selected = games.get(new Random().nextInt(games.size()));
         started = true;
-        
-        for(PlayerInterface pl : players.keySet()) {
-            pl.startGame(selected.getValues());
+        horaAtual = LocalDateTime.now();
+        for (PlayerInterface pl : players.keySet()) {
+            pl.startGame(selected.getValues(), horaAtual);
         }
     }
 
@@ -179,11 +178,11 @@ public class Server extends UnicastRemoteObject implements SudokuInterface {
 
     @Override
     public synchronized void isWinner(PlayerInterface player) throws RemoteException {
-         if(players.get(player).getScore() == selected.getScore()) {
-             for (PlayerInterface i : players.keySet()) {
+        if (players.get(player).getScore() == selected.getScore()) {
+            for (PlayerInterface i : players.keySet()) {
                 i.gameEnd(players.get(player).getName());
             }
-         }
+        }
     }
-    
+
 }
