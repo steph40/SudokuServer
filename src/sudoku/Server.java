10f4f4ff0785f05fd47601c638ff39e17ee1ed57
InesAguas <inocas_99@hotmail.com>
2023-06-24 12:50:22 +0100

    3 sudokus

--- src/sudoku/Server.java
+++ src/sudoku/Server.java
@@ -33,41 +33,74 @@ public class Server extends UnicastRemoteObject implements SudokuInterface {
 
     //lista de jogadores com o nome pelo menos
     public static void main(String[] args) {
-        int[][] solved1 = {{5, 3, 4, 6, 7, 8, 9, 1, 2},
-        {6, 7, 2, 1, 9, 5, 3, 4, 8},
-        {1, 9, 8, 3, 4, 2, 5, 6, 7},
-        {8, 5, 9, 7, 6, 1, 4, 2, 3},
-        {4, 2, 6, 8, 5, 3, 7, 9, 1},
-        {7, 1, 3, 9, 2, 4, 8, 5, 6},
-        {9, 6, 1, 5, 3, 7, 2, 8, 4},
-        {2, 8, 7, 4, 1, 9, 6, 3, 5},
-        {3, 4, 5, 2, 8, 6, 1, 7, 9}};
+        int[][] solved1 = {{6, 5, 9, 3, 1, 4, 2, 8, 7},
+        {1, 8, 7, 6, 5, 2, 4, 3, 9},
+        {2, 3, 4, 8, 9, 7, 5, 1, 6},
+        {4, 2, 6, 1, 3, 5, 9, 7, 8},
+        {8, 7, 1, 9, 4, 6, 3, 5, 2},
+        {5, 9, 3, 2, 7, 8, 6, 4, 1},
+        {3, 1, 2, 5, 8, 9, 7, 6, 4},
+        {7, 6, 5, 4, 2, 1, 8, 9, 3},
+        {9, 4, 8, 7, 6, 3, 1, 2, 5}};
 
-        int[][] unsolved1 = {{5, 0, 4, 6, 7, 8, 9, 1, 2},
-        {6, 7, 2, 1, 9, 5, 3, 0, 8},
-        {1, 9, 8, 3, 4, 2, 5, 6, 7},
-        {8, 0, 9, 7, 6, 1, 4, 2, 3},
-        {4, 2, 6, 8, 5, 3, 7, 9, 1},
-        {7, 1, 3, 9, 2, 4, 8, 5, 6},
-        {9, 6, 1, 5, 3, 7, 2, 8, 4},
-        {2, 8, 7, 4, 1, 9, 6, 3, 5},
-        {3, 4, 5, 2, 8, 6, 1, 7, 9}};
+        int[][] unsolved1 = {{6, 5, 9, 0, 1, 0, 2, 8, 0},
+        {1, 0, 0, 0, 5, 0, 0, 3, 0},
+        {2, 0, 0, 8, 0, 0, 0, 1, 0},
+        {0, 0, 0, 1, 3, 5, 0, 7, 0},
+        {8, 0, 0, 9, 0, 0, 0, 0, 2},
+        {0, 0, 3, 0, 7, 8, 6, 4, 0},
+        {3, 0, 2, 0, 0, 9, 0, 0, 4},
+        {0, 0, 0, 0, 0, 1, 8, 0, 0},
+        {0, 0, 8, 7, 6, 0, 0, 0, 0}};
 
-        int[][] unsolved2 = {{5, 0, 4, 6, 7, 8, 9, 1, 2},
-        {6, 7, 2, 1, 9, 5, 3, 4, 8},
-        {1, 9, 8, 3, 4, 2, 5, 6, 7},
-        {8, 5, 9, 7, 6, 1, 4, 2, 3},
-        {4, 2, 6, 8, 5, 3, 7, 9, 1},
-        {7, 1, 3, 9, 2, 4, 8, 5, 6},
-        {9, 6, 1, 5, 3, 7, 2, 8, 4},
-        {2, 8, 7, 4, 1, 9, 6, 3, 5},
-        {3, 4, 5, 2, 8, 6, 1, 7, 9}};
+        int[][] solved2 = {{7, 3, 6, 8, 1, 4, 9, 2, 5},
+        {5, 4, 9, 6, 3, 2, 1, 7, 8},
+        {2, 1, 8, 9, 5, 7, 3, 6, 4},
+        {6, 5, 4, 2, 8, 1, 7, 9, 3},
+        {9, 2, 3, 7, 4, 6, 8, 5, 1},
+        {1, 8, 7, 3, 9, 5, 2, 4, 6},
+        {8, 7, 5, 4, 2, 3, 6, 1, 9},
+        {3, 6, 1, 5, 7, 9, 4, 8, 2},
+        {4, 9, 2, 1, 6, 8, 5, 3, 7}};
+        
+        int[][] unsolved2 = {{7, 3, 6, 8, 1, 4, 0, 0, 5},
+        {0, 0, 9, 6, 0, 2, 0, 7, 8},
+        {2, 1, 0, 9, 0, 0, 3, 6, 0},
+        {6, 0, 4, 2, 0, 1, 7, 9, 0},
+        {0, 2, 0, 7, 0, 0, 0, 5, 1},
+        {0, 8, 7, 0, 9, 5, 2, 0, 6},
+        {8, 0, 5, 0, 2, 3, 6, 1, 9},
+        {0, 0, 0, 5, 0, 0, 4, 0, 2},
+        {4, 9, 2, 1, 0, 8, 5, 0, 0}};
+        
+        int[][] solved3 = {{3, 5, 1, 4, 9, 8, 7, 2, 6},
+        {2, 4, 9, 7, 6, 5, 1, 3, 8},
+        {6, 7, 8, 1, 3, 2, 9, 5, 4},
+        {8, 9, 4, 5, 2, 3, 6, 1, 7},
+        {1, 3, 2, 6, 8, 7, 5, 4, 9},
+        {5, 6, 7, 9, 1, 4, 2, 8, 3},
+        {7, 1, 3, 2, 4, 6, 8, 9, 5},
+        {4, 2, 6, 8, 5, 9, 3, 7, 1},
+        {9, 8, 5, 3, 7, 1, 4, 6, 2}};
+        
+        int[][] unsolved3 = {{3, 5, 1, 0, 0, 8, 7, 0, 6},
+        {0, 4, 0, 7, 0, 0, 1, 0, 0},
+        {0, 7, 0, 0, 0, 0, 9, 5, 4},
+        {8, 0, 4, 0, 2, 0, 6, 0, 0},
+        {0, 3, 2, 0, 0, 0, 0, 0, 0},
+        {0, 6, 7, 0, 1, 0, 0, 0, 3},
+        {7, 1, 0, 0, 4, 6, 0, 0, 5},
+        {0, 0, 6, 0, 5, 9, 0, 7, 0},
+        {9, 8, 0, 3, 0, 0, 4, 0, 2}};
+        
 
         Sudoku game1 = new Sudoku(solved1, unsolved1);
-        Sudoku game2 = new Sudoku(solved1, unsolved2);
+        Sudoku game2 = new Sudoku(solved2, unsolved2);
+        Sudoku game3 = new Sudoku(solved3, unsolved3);
 
         games.add(game1);
         games.add(game2);
+        games.add(game3);
 
         try {
             Registry reg = LocateRegistry.createRegistry(1099);
@@ -104,7 +137,7 @@ public class Server extends UnicastRemoteObject implements SudokuInterface {
         return true;
     }
 
-    private synchronized void notifyPlayerJoined(String username) throws RemoteException {
+    private void notifyPlayerJoined(String username) throws RemoteException {
         for (PlayerInterface i : players.keySet()) {
             i.playerJoined(username);
         }
@@ -192,7 +225,7 @@ public class Server extends UnicastRemoteObject implements SudokuInterface {
     }
 
     @Override
-    public synchronized Boolean getGameStatus() throws RemoteException {
+    public Boolean getGameStatus() throws RemoteException {
         return started;
     }
 
